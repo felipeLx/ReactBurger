d60c379f6c12578e2f47aea836e711383db3f99b
felipeLx <felipealvesfinanceiro@gmail.com>
2019-12-05 22:06:58 -0300

    refactor classes with redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -111,7 +112,7 @@ class ContactData extends Component {
     }
 
     orderHandler = (event) => {
-        console.log(this.props.ingredients);
+        console.log(this.props.ings);
         event.preventDefault();
         this.setState( { loading: true } );
         const formData = {};
@@ -119,7 +120,7 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
         }
@@ -190,4 +191,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    };
+}
+
+export default connect(mapStateToProps) (ContactData);
